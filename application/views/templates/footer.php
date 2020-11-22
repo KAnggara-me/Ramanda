25e1835c06aec0335fb281bc560820f97f3696c3
Call Vin <kanggara75@gmail.com>
2020-11-22 21:06:23 +0700

    Sebelum menambahkan fitur

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,10 +1,10 @@
             <!-- Footer -->
             <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Web Programming UNPAS <?= date('Y'); ?></span>
-                    </div>
+              <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                  <span>Copyright &copy; Web Programming UNPAS <?= date('Y'); ?></span>
                 </div>
+              </div>
             </footer>
             <!-- End of Footer -->
 
@@ -16,26 +16,26 @@
 
             <!-- Scroll to Top Button-->
             <a class="scroll-to-top rounded" href="#page-top">
-                <i class="fas fa-angle-up"></i>
+              <i class="fas fa-angle-up"></i>
             </a>
 
             <!-- Logout Modal-->
             <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                        <div class="modal-footer">
-                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                            <a class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</a>
-                        </div>
-                    </div>
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                  <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</a>
+                  </div>
                 </div>
+              </div>
             </div>
 
             <!-- Bootstrap core JavaScript-->
@@ -49,32 +49,31 @@
             <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
             <script>
-                $('.custom-file-input').on('change', function() {
-                    let fileName = $(this).val().split('\\').pop();
-                    $(this).next('.custom-file-label').addClass("selected").html(fileName);
+              $('.custom-file-input').on('change', function() {
+                let fileName = $(this).val().split('\\').pop();
+                $(this).next('.custom-file-label').addClass("selected").html(fileName);
+              });
+
+
+
+              $('.form-check-input').on('click', function() {
+                const menuId = $(this).data('menu');
+                const roleId = $(this).data('role');
+
+                $.ajax({
+                  url: "<?= base_url('admin/changeaccess'); ?>",
+                  type: 'post',
+                  data: {
+                    menuId: menuId,
+                    roleId: roleId
+                  },
+                  success: function() {
+                    document.location.href = "<?= base_url('admin/roleaccess/'); ?>" + roleId;
+                  }
                 });
 
-
-
-                $('.form-check-input').on('click', function() {
-                    const menuId = $(this).data('menu');
-                    const roleId = $(this).data('role');
-
-                    $.ajax({
-                        url: "<?= base_url('admin/changeaccess'); ?>",
-                        type: 'post',
-                        data: {
-                            menuId: menuId,
-                            roleId: roleId
-                        },
-                        success: function() {
-                            document.location.href = "<?= base_url('admin/roleaccess/'); ?>" + roleId;
-                        }
-                    });
-
-                });
+              });
             </script>
-
             </body>
 
-            </html> 
+            </html>
