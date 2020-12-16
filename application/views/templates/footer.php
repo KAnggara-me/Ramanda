184bfb825644884a3e9341d4f5c40a4db0d740b6
Call Vin <kanggara75@gmail.com>
2020-12-16 23:34:57 +0700

    change indent space to 2 from 4

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,226 +1,226 @@
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-              <div class="container my-auto">
-                <div class="copyright text-center my-auto">
-                  <span>Copyright &copy; Web Programming UNPAS <?= date('Y'); ?></span>
-                </div>
-              </div>
-            </footer>
-            <!-- End of Footer -->
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright &copy; Web Programming UNPAS <?= date('Y'); ?></span>
+    </div>
+  </div>
+</footer>
+<!-- End of Footer -->
 
-            </div>
-            <!-- End of Content Wrapper -->
+</div>
+<!-- End of Content Wrapper -->
 
-            </div>
-            <!-- End of Page Wrapper -->
+</div>
+<!-- End of Page Wrapper -->
 
-            <!-- Scroll to Top Button-->
-            <a class="scroll-to-top rounded" href="#page-top">
-              <i class="fas fa-angle-up"></i>
-            </a>
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+  <i class="fas fa-angle-up"></i>
+</a>
 
-            <!-- Logout Modal-->
-            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">×</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                  <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</a>
-                  </div>
-                </div>
-              </div>
-            </div>
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+        <a class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</a>
+      </div>
+    </div>
+  </div>
+</div>
 
-            <!-- Bootstrap core JavaScript-->
-            <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
-            <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Bootstrap core JavaScript-->
+<script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-            <!-- Core plugin JavaScript-->
-            <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
-            <!-- Custom scripts for all pages-->
-            <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
-            <script>
-              $('.custom-file-input').on('change', function() {
-                let fileName = $(this).val().split('\\').pop();
-                $(this).next('.custom-file-label').addClass("selected").html(fileName);
-              });
+<script>
+  $('.custom-file-input').on('change', function() {
+    let fileName = $(this).val().split('\\').pop();
+    $(this).next('.custom-file-label').addClass("selected").html(fileName);
+  });
 
 
 
-              $('.form-check-input').on('click', function() {
-                const menuId = $(this).data('menu');
-                const roleId = $(this).data('role');
+  $('.form-check-input').on('click', function() {
+    const menuId = $(this).data('menu');
+    const roleId = $(this).data('role');
 
-                $.ajax({
-                  url: "<?= base_url('admin/changeaccess'); ?>",
-                  type: 'post',
-                  data: {
-                    menuId: menuId,
-                    roleId: roleId
-                  },
-                  success: function() {
-                    document.location.href = "<?= base_url('admin/roleaccess/'); ?>" + roleId;
-                  }
-                });
+    $.ajax({
+      url: "<?= base_url('admin/changeaccess'); ?>",
+      type: 'post',
+      data: {
+        menuId: menuId,
+        roleId: roleId
+      },
+      success: function() {
+        document.location.href = "<?= base_url('admin/roleaccess/'); ?>" + roleId;
+      }
+    });
 
-              });
-            </script>
-            <!-- Chart Script -->
-            <script>
-              function number_format(number, decimals, dec_point, thousands_sep) {
-                number = (number + '').replace(',', '').replace(' ', '');
-                var n = !isFinite(+number) ? 0 : +number,
-                  prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-                  sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-                  dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-                  s = '',
-                  toFixedFix = function(n, prec) {
-                    var k = Math.pow(10, prec);
-                    return '' + Math.round(n * k) / k;
-                  };
-                // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-                s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-                if (s[0].length > 3) {
-                  s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-                }
-                if ((s[1] || '').length < prec) {
-                  s[1] = s[1] || '';
-                  s[1] += new Array(prec - s[1].length + 1).join('0');
-                }
-                return s.join(dec);
-              }
+  });
+</script>
+<!-- Chart Script -->
+<script>
+  function number_format(number, decimals, dec_point, thousands_sep) {
+    number = (number + '').replace(',', '').replace(' ', '');
+    var n = !isFinite(+number) ? 0 : +number,
+      prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+      sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+      dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+      s = '',
+      toFixedFix = function(n, prec) {
+        var k = Math.pow(10, prec);
+        return '' + Math.round(n * k) / k;
+      };
+    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    if (s[0].length > 3) {
+      s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+    if ((s[1] || '').length < prec) {
+      s[1] = s[1] || '';
+      s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
+  }
 
-              var ctx = document.getElementById("myAreaChart");
-              var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                  labels: [
-                    <?php foreach ($obs as $t) : ?>
-                      <?= '"' . date('d/m/y', $t['time']) . '",'; ?>
-                    <?php endforeach; ?>
-                  ],
-                  datasets: [{
-                      label: 'Jarak Tempuh Harian',
-                      lineTension: 0.3,
-                      pointRadius: 5,
-                      pointBackgroundColor: "rgba(255, 99, 132, 1)",
-                      pointBorderColor: "rgba(255, 99, 132, 1)",
-                      pointHoverRadius: 3,
-                      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-                      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-                      pointHitRadius: 10,
-                      pointBorderWidth: 2,
-                      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                      borderColor: 'rgba(255,99,132,1)',
-                      borderWidth: 2,
-                      data: [
-                        <?php foreach ($obs as $t) : ?>
-                          <?= '"' . $t['data'] / 7050 . '",'; ?>
-                        <?php endforeach; ?>
-                      ],
-                    },
-                    {
-                      label: 'Jarak Tempuh Total',
-                      lineTension: 0.3,
-                      backgroundColor: "rgba(78, 115, 223, 0.05)",
-                      borderColor: "rgba(78, 115, 223, 0.5)",
-                      pointRadius: 3,
-                      pointBackgroundColor: "rgba(78, 115, 223, 1)",
-                      pointBorderColor: "rgba(78, 115, 223, 1)",
-                      pointHoverRadius: 3,
-                      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-                      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-                      pointHitRadius: 10,
-                      pointBorderWidth: 2,
-                      borderWidth: 1,
-                      data: [
-                        <?php foreach ($obs as $t) : ?>
-                          <?= '"' . $t['total'] / 7050 . '",'; ?>
-                        <?php endforeach; ?>
-                      ],
-                    },
-                  ]
-                },
-                options: {
-                  maintainAspectRatio: false,
-                  layout: {
-                    padding: {
-                      left: 10,
-                      right: 25,
-                      top: 25,
-                      bottom: 0
-                    }
-                  },
-                  scales: {
-                    xAxes: [{
-                      time: {
-                        unit: 'date'
-                      },
-                      gridLines: {
-                        color: "rgb(234, 236, 244)",
-                        zeroLineColor: "rgb(234, 236, 244)",
-                        drawBorder: true,
-                        // borderDash: [2],
-                        // zeroLineBorderDash: [2]
-                      },
-                      // ticks: {
-                      //   maxTicksLimit: 10
-                      // }
-                    }],
-                    yAxes: [{
-                      ticks: {
-                        // maxTicksLimit: 5,
-                        padding: 10,
-                        // Include a dollar sign in the ticks
-                        // callback: function(value, index, values) {
-                        //   return '' + number_format(value);
-                        // }
-                      },
-                      gridLines: {
-                        color: "rgb(234, 236, 244)",
-                        zeroLineColor: "rgb(234, 236, 244)",
-                        drawBorder: true,
-                        // borderDash: [2],
-                        // zeroLineBorderDash: [2]
-                      }
-                    }],
-                  },
-                  legend: {
-                    display: false
-                  },
-                  tooltips: {
-                    backgroundColor: "rgb(255,255,255)",
-                    bodyFontColor: "#858796",
-                    titleMarginBottom: 10,
-                    titleFontColor: '#6e707e',
-                    titleFontSize: 14,
-                    borderColor: '#dddfeb',
-                    borderWidth: 1,
-                    xPadding: 15,
-                    yPadding: 15,
-                    displayColors: true,
-                    intersect: false,
-                    mode: 'index',
-                    caretPadding: 10,
-                    callbacks: {
-                      label: function(tooltipItem, chart) {
-                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-                        return datasetLabel + ': ' + number_format(tooltipItem.yLabel) + ' KM';
-                      }
-                    }
-                  }
-                }
-              });
-            </script>
-            </body>
+  var ctx = document.getElementById("myAreaChart");
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        <?php foreach ($obs as $t) : ?>
+          <?= '"' . date('d/m/y', $t['time']) . '",'; ?>
+        <?php endforeach; ?>
+      ],
+      datasets: [{
+          label: 'Jarak Tempuh Harian',
+          lineTension: 0.3,
+          pointRadius: 5,
+          pointBackgroundColor: "rgba(255, 99, 132, 1)",
+          pointBorderColor: "rgba(255, 99, 132, 1)",
+          pointHoverRadius: 3,
+          pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 2,
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 2,
+          data: [
+            <?php foreach ($obs as $t) : ?>
+              <?= '"' . $t['data'] / 7050 . '",'; ?>
+            <?php endforeach; ?>
+          ],
+        },
+        {
+          label: 'Jarak Tempuh Total',
+          lineTension: 0.3,
+          backgroundColor: "rgba(78, 115, 223, 0.05)",
+          borderColor: "rgba(78, 115, 223, 0.5)",
+          pointRadius: 3,
+          pointBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointBorderColor: "rgba(78, 115, 223, 1)",
+          pointHoverRadius: 3,
+          pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 2,
+          borderWidth: 1,
+          data: [
+            <?php foreach ($obs as $t) : ?>
+              <?= '"' . $t['total'] / 7050 . '",'; ?>
+            <?php endforeach; ?>
+          ],
+        },
+      ]
+    },
+    options: {
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          left: 10,
+          right: 25,
+          top: 25,
+          bottom: 0
+        }
+      },
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'date'
+          },
+          gridLines: {
+            color: "rgb(234, 236, 244)",
+            zeroLineColor: "rgb(234, 236, 244)",
+            drawBorder: true,
+            // borderDash: [2],
+            // zeroLineBorderDash: [2]
+          },
+          // ticks: {
+          //   maxTicksLimit: 10
+          // }
+        }],
+        yAxes: [{
+          ticks: {
+            // maxTicksLimit: 5,
+            padding: 10,
+            // Include a dollar sign in the ticks
+            // callback: function(value, index, values) {
+            //   return '' + number_format(value);
+            // }
+          },
+          gridLines: {
+            color: "rgb(234, 236, 244)",
+            zeroLineColor: "rgb(234, 236, 244)",
+            drawBorder: true,
+            // borderDash: [2],
+            // zeroLineBorderDash: [2]
+          }
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        titleMarginBottom: 10,
+        titleFontColor: '#6e707e',
+        titleFontSize: 14,
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: true,
+        intersect: false,
+        mode: 'index',
+        caretPadding: 10,
+        callbacks: {
+          label: function(tooltipItem, chart) {
+            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+            return datasetLabel + ': ' + number_format(tooltipItem.yLabel) + ' KM';
+          }
+        }
+      }
+    }
+  });
+</script>
+</body>
 
-            </html>
+</html>
